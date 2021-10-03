5aef446cecaa862499aca2f19f47929c90a80c86
Matheus Mitestainer <matheusmitestainer@outlook.com>
2021-10-02 23:25:26 -0300

    Add Checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,50 @@
+import { InputHTMLAttributes, useState } from 'react'
+import * as S from './styles'
+
+export type CheckboxProps = {
+  label?: string
+  labelFor?: string
+  labelColor?: 'white' | 'black'
+  onCheck?: (status: boolean) => void
+  isChecked?: boolean
+  value?: string | ReadonlyArray<string> | number
+} & InputHTMLAttributes<HTMLInputElement>
+
+const Checkbox = ({
+  label,
+  labelFor = '',
+  labelColor = 'white',
+  onCheck,
+  isChecked = false,
+  value,
+  ...props
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(isChecked)
+
+  const handleChange = () => {
+    const status = !checked
+    setChecked(status)
+
+    !!onCheck && onCheck(status)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={handleChange}
+        checked={checked}
+        value={value}
+        {...props}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
+
+export default Checkbox
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -100,7 +100,7 @@ export const MenuFull = styled.nav<MenuFullProps>`
     height: 100vh;
     overflow: hidden;
     opacity: ${isOpen ? 1 : 0};
-    transition: opacity 0.3s ease-in-out;
+    transition: opacity ${theme.transition.default};
     pointer-events: ${isOpen ? 'all' : 'none'};
 
     > svg {
@@ -126,12 +126,12 @@ export const MenuFull = styled.nav<MenuFullProps>`
       font-size: ${theme.font.sizes.xlarge};
       margin-bottom: ${theme.spacings.small};
       transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
-      transition: transform 0.3s ease-in-out;
+      transition: transform ${theme.transition.default};
     }
 
     ${RegisterBox} {
       transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
-      transition: transform 0.3s ease-in-out;
+      transition: transform ${theme.transition.default};
     }
   `}
 `
