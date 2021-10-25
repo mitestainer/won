2cafc161d0ffd3b48aae41b24c77d568996a151b
Matheus Mitestainer <matheusmitestainer@outlook.com>
2021-10-25 19:27:57 -0300

    feat: add signup + udpdate form components

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -26,8 +26,14 @@ describe('<TextField />', () => {
   })
 
   it('Changes its value when typing', async () => {
-    const onInput = jest.fn()
-    render(<TextField onInput={onInput} label="TextField" name="textField" />)
+    const onInputChange = jest.fn()
+    render(
+      <TextField
+        onInputChange={onInputChange}
+        label="TextField"
+        name="textField"
+      />
+    )
 
     const input = screen.getByRole('textbox')
     const text = 'This is my new text'
@@ -35,9 +41,9 @@ describe('<TextField />', () => {
 
     await waitFor(() => {
       expect(input).toHaveValue(text)
-      expect(onInput).toHaveBeenCalledTimes(text.length)
+      expect(onInputChange).toHaveBeenCalledTimes(text.length)
     })
-    expect(onInput).toHaveBeenCalledWith(text)
+    expect(onInputChange).toHaveBeenCalledWith(text)
   })
 
   it('Is accessible by tab', () => {
@@ -71,11 +77,11 @@ describe('<TextField />', () => {
   })
 
   it('should not allow changing its values when disabled', async () => {
-    const onInput = jest.fn()
+    const onInputChange = jest.fn()
 
     render(
       <TextField
-        onInput={onInput}
+        onInputChange={onInputChange}
         label="textField"
         name="textField"
         disabled
@@ -92,7 +98,7 @@ describe('<TextField />', () => {
       expect(input).not.toHaveValue(text)
     })
 
-    expect(onInput).not.toHaveBeenCalled()
+    expect(onInputChange).not.toHaveBeenCalled()
   })
 
   it('should not be accessible by tab if disabled', () => {
