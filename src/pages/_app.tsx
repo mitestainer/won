ea70a21d32e6a2aba3f8df632af5aa0ffe19aaeb
Matheus Mitestainer <matheusmitestainer@outlook.com>
2021-10-31 19:17:13 -0300

    feat: create useWishlist hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 import { useApollo } from 'utils/apollo'
+import { WishlistProvider } from 'hooks/use-wishlist'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
@@ -20,22 +21,24 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta name="theme-color" content="#06092B" />
-              <meta name="description" content="A great game store!" />
-            </Head>
-            <GlobalStyles />
-            <NextNProgress
-              color="#F231A5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={3}
-            />
-            <Component {...pageProps} />
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#06092B" />
+                <meta name="description" content="A great game store!" />
+              </Head>
+              <GlobalStyles />
+              <NextNProgress
+                color="#F231A5"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={3}
+              />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
