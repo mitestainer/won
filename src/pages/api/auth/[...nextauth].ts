cfc4256cd3135f81c6a0feb11ef3c7996dc452e1
Matheus Mitestainer <matheusmitestainer@outlook.com>
2021-11-05 16:47:38 -0300

    update: fixing next/auth types

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import NextAuth from 'next-auth'
+import NextAuth, { Session, User } from 'next-auth'
 import Providers from 'next-auth/providers'
-import { GenericObject } from 'next-auth/_utils'
+import { JWT } from 'next-auth/jwt'
 
 type CredentialTypes = {
   email: string
@@ -36,13 +36,13 @@ const options = {
     })
   ],
   callbacks: {
-    session: async (session: GenericObject, user: GenericObject) => {
+    session: async (session: Session, user: User) => {
       session.jwt = user.jwt
       session.id = user.id
 
       return Promise.resolve(session)
     },
-    jwt: async (token: GenericObject, user: GenericObject) => {
+    jwt: async (token: JWT, user: User) => {
       if (user) {
         token.id = user.id
         token.email = user.email
